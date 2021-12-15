51bd288849beed6ba3fd3634e71f3dc339a5fdce
marcell0lopes <marcell0lopes@outlook.com>
2021-12-15 05:38:02 -0300

    Ultimos estilos antes de escalar o projeto

--- seeds/index.js
+++ seeds/index.js
@@ -1,31 +1,37 @@
-const mongoose = require('mongoose');
-const cities = require('./cities')
-const {places, descriptors} = require('./seedHelpers');
-const Campground = require('../models/campground')
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'mongoose connection error:'));
-db.once('open', () => {
-	console.log('Database Connected');
+db.on("error", console.error.bind(console, "mongoose connection error:"));
+db.once("open", () => {
+  console.log("Database Connected");
 });
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)]
-
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for(let i = 0; i <50; i++){
-        const random1000 = Math.floor(Math.random()*1000);
-        const camp =new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    //setup
+    const sample = (array) => array[Math.floor(Math.random() * array.length)];
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
 
-    }
-}
+    //seed data into campground
+    const camp = new Campground({
+      image: "https://source.unsplash.com/collection/483251",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!",
+      price,
+    });
+    await camp.save();
+  }
+};
 
-seedDB().then(() =>{
-    mongoose.connection.close();
-})
+seedDB().then(() => {
+  mongoose.connection.close();
+});
